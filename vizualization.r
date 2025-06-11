4bee117431bf952cde6aefc257da83a9dbdcdabf
AkoiCPAL <akoi@cpal.org>
2025-06-11 14:32:35 -0500

    added Poland

--- vizualization.r
+++ vizualization.r
@@ -2,12 +2,12 @@ library(ggplot2)
 library(maps)
 library(tidyverse)
 
-# Read your CSV file into a data frame called 'poland'
-poland <- read_csv("Apartment_Prices_Poland/apartments_pl_2024_06.csv")
+poland <- read_csv("Apartment_Prices_Poland/apartments_pl_2024_01.csv")
 
-# Plot apartment locations on the map of Poland
-ggplot() +
-    borders("poland", colour = "gray50", fill = "gray90") +
-    geom_point(data = poland, aes(x = longitude, y = latitude, color = price), alpha = 0.7) +
-    scale_color_viridis_c() +
-    labs(title = "Apartment Prices in Poland", x = "Longitude", y = "Latitude", color = "Price")
+poland_map_data <- map_data("world", region = "Poland")
+
+ggplot(poland_map_data, aes(x = long, y = lat, fill = 1)) +
+    geom_polygon(aes(fill = 1), color = "white", alpha = 0.5) +
+    geom_point(data = poland, aes(x = longitude, y = latitude), color = "red", size = 1, alpha = 0.6) +
+    coord_fixed(1.3) +
+    theme_minimal()
